59ae4eee321eb8c5afad56650273039817ac09e1
ohnishant <nishant.mishra550@gmail.com>
2023-03-24 09:40:26 +0530

    perf: day2 merge_sorted_array

--- nishant_mishra/day_2/merge_sorted_array.cpp
+++ nishant_mishra/day_2/merge_sorted_array.cpp
@@ -12,10 +12,6 @@ public:
             if(nums1[i] > nums2[j]) { nums1[k--] = nums1[i--]; }
             else { nums1[k--] = nums2[j--]; }
         }
-        while (i>=0) {
-            nums1[k--] = nums1[i--];
-        }
-
         while (j >= 0) {
             nums1[k--] = nums2[j--];
         }
