210c2602a661849481d869e9801ff6a6a2f9f1bf
Rohit Kumar <86485476+Kumarohit45@users.noreply.github.com>
2023-03-26 23:37:09 +0530

    Modified Question 1 and 2 of Day 3.

--- ROHIT KUMAR/Day 4/Question_2.cpp
+++ ROHIT KUMAR/Day 4/Question_2.cpp
@@ -1,25 +1,9 @@
-class Solution {
-public:
-    double myPow(double x, int n) {
-        double ans=x, res=0;
-        if (x == 1){
-            res = 1;
+
+        double result = 1.0;
+        for(int i=n; i; i/=2){
+            if (i & 1) result *= x;
+            x = x * x;
         }
-        else {
-            if (n > 0){
-                for (unsigned int i=2; i<=n; i++)
-                    ans = ans*x;
-                res = ans;
-            }
-            else if (n == 0){
-                res = 1;
-            }
-            else if (n < 0){
-                for (int i=n+1; i<0; i++)
-                    ans = ans*x;
-                res = 1/ans;
-            }
-        }
-        return res;
+        return n >= 0 ? result : 1/result; 
     }
 };
