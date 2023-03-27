b4adb04af2dba506228b3a5a6a7d285c63b63949
Akshat Gupta <a.akshat903@gmail.com>
2023-03-27 21:21:29 +0530

    files added

---
+++ Akshat Gupta/Day-5B.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s;
+        for(int i=0; i<nums.size(); i++){
+            s.insert(nums[i]);
+        }
+        if(nums.size() != s.size()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
