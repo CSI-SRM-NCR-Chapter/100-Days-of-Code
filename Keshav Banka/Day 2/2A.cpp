3113413ebf4f8724e666c70387f70bbd97ad3ad6
EKANSH SRIVASTAVA <116584951+ekansh1818@users.noreply.github.com>
2023-03-31 00:11:47 +0530

    WRONG FILES

--- Keshav Banka/Day 2/2A.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for (auto ele : nums) ans ^= ele;
-        return ans;
-    }
-};
--- Vershit-Chaudhary/Day2-Q1.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun singleNumber(nums: IntArray): Int {
-        var res=0;
-        for(i in nums.indices)
-            res=res xor nums[i];
-        
-        return res;
-    }
-}
