9698e8440d1ff2d1f30d064de32cde1118a2966f
Tanmay Jaiswal <74481733+sequel-tj@users.noreply.github.com>
2023-03-31 23:58:25 +0530

    missing number

--- Tanmay Jaiswal/day-10/10b.cpp
+++ Tanmay Jaiswal/day-10/10b.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+    int missingNumber(vector<int>& nums) 
+    {
         int n = nums.size();
         int overallSum = (n*(n+1))/2;
         int sum = 0;
