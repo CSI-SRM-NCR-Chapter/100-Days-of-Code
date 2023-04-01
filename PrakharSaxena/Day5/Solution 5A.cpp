ffafe61ab85bab8cfe7322b2595f7941525ffa05
Someone-who-loves-coding <ps2284@srmist.edu.in>
2023-04-01 18:11:01 +0530

    Solution 5A

---
+++ PrakharSaxena/Day5/Solution 5A.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+            return nums[n/2];
+        
+    }
+};
