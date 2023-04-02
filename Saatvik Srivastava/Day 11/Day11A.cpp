cfc800e4eaacb6dad7649f9e11318a1f09f03828
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-04-02 23:46:28 +0530

    Hamming Distance

---
+++ Saatvik Srivastava/Day 11/Day11A.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count =0;
+        while(x!=0 || y!= 0){
+            if(x%2 != y%2)
+                count++;
+            x=x/2;
+            y = y/2;
+        }
+        return count;
+    }
+};
+
+
+
