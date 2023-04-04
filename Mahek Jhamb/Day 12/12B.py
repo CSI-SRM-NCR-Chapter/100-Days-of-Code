81bb8176d542fda214611b1bcf76784dd81fc69c
Mahek Jhamb <mahekjhamb043@gmail.com>
2023-04-04 22:44:42 +0530

    Solutions for Day12

---
+++ Mahek Jhamb/Day 12/12B.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        note, mag = Counter(ransomNote), Counter(magazine)
+        if note & mag == note: 
+            return True
+        return False
