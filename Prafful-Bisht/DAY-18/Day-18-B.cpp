c9fd52d0ca408d3b062872bcb0b92a6dfa199e5f
iampraffulbisht <114369813+iampraffulbisht@users.noreply.github.com>
2023-04-10 23:59:21 +0530

    Day-18 and 19

---
+++ Prafful-Bisht/DAY-18/Day-18-B.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long s=0,e=x;
+        int ans;
+        while(s<=e){
+            long long m = s+(e-s)/2;
+            if(m*m<=x){
+                ans = m;
+                s = m+1;
+            }
+            else{
+                e=m-1;
+            }
+        }
+        return ans;
+    }
+};
