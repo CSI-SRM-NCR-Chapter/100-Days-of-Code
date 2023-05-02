a90e6e290efa5710f8c0e1bdf7839d6f5eb2bda9
Prashant Kumar Singh <81946035+Prashant160420@users.noreply.github.com>
2023-05-03 00:25:05 +0530

    Create 41b.cpp

---
+++ Prashant Kumar Singh/day-41/41b.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st; 
+        for(auto i:s){
+            if(i=='(' or i=='{' or i=='[') st.push(i);  
+            else{
+                if(st.empty() || (st.top()=='(' && i!=')') || (st.top()=='{' && i!='}') || (st.top()=='[' && i!=']')) return false;
+                st.pop(); 
+            }
+        }
+        return st.empty();  
+    }
+};
