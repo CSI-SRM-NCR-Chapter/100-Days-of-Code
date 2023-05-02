0ee65d0c68e9e11790098f74b21c647d282c1ace
Ridam Krishna <94859397+ridamEXE@users.noreply.github.com>
2023-05-02 23:31:58 +0530

    Valid Parentheses

---
+++ ridam krishna/day41/41b.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                st.push(s[i]);
+            else if(st.empty()==false){
+                if(s[i]==')' && st.top()=='(') st.pop();
+                else if(s[i]==']' && st.top()=='[') st.pop();
+                else if(s[i]=='}' && st.top()=='{') st.pop();
+                else return false;
+            }
+            else return false;
+            
+        }
+        return st.empty();
+    }
+};
