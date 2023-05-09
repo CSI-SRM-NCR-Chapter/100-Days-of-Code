7cbcf026d9ef72535d94d67cc7b7bd8daace322c
Ridam Krishna <94859397+ridamEXE@users.noreply.github.com>
2023-05-09 19:40:43 +0530

    Guess Number Higher or Lower

---
+++ ridam krishna/day48/48a.cpp
@@ -0,0 +1,29 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int left = 1, right = n;
+        while (left <= n) {
+            int mid = left + (right - left) / 2;
+            int g = guess(mid);
+            if (g == 0) {
+                return mid;
+            }
+            else if (g == 1) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return 0;
+    }
+};
