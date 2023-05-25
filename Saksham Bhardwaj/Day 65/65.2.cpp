7bb7c0a3a266e5c749df27b86e4701b17760ffb8
Saksham Bhardwaj <109824318+sakshamxb@users.noreply.github.com>
2023-05-26 02:58:34 +0530

    Day 65th of 100 : Problem 1 Added!

---
+++ Saksham Bhardwaj/Day 65/65.2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int nextNumber(int n){
+        int newNumber = 0;
+        while(n!=0){
+            int num = n%10;
+            newNumber += num*num;
+            n = n/10;
+        }
+        return newNumber;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        while(n!=1 && !set.count(n)){
+            set.insert(n);
+            n = nextNumber(n);
+        }
+        return n==1;
+    }
+};
