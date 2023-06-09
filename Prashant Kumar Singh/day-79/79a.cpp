1c747072f3102e82c1e6f93557a24dd6204f9123
Prashant Kumar Singh <81946035+Prashant160420@users.noreply.github.com>
2023-06-09 23:18:35 +0530

    Create 79a.cpp

---
+++ Prashant Kumar Singh/day-79/79a.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void helper(int idx, int k,vector<int>&current,int n){
+        if(current.size()==k){
+            ans.push_back(current);
+            return;
+        }
+        
+        for(int i=idx;i<n+1;i++){
+            current.push_back(i);
+            helper(i+1,k,current,n);
+            current.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>current;
+        helper(1,k,current,n);
+        return ans;
+    }
+};
