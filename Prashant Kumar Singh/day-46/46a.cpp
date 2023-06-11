ccf35e6155b3a5477f2c765507f825cb757973db
Prashant Kumar Singh <81946035+Prashant160420@users.noreply.github.com>
2023-06-11 21:36:24 +0530

    Update 46a.cpp

--- Prashant Kumar Singh/day-46/46a.cpp
+++ Prashant Kumar Singh/day-46/46a.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-    vector<int> result(nums.size());
-    int left = 0;
-    int right = nums.size() - 1;
-    int index = right;
-
-    while (left <= right) {
-        int leftSquare = nums[left] * nums[left];
-        int rightSquare = nums[right] * nums[right];
-
-        if (leftSquare > rightSquare) {
-            result[index] = leftSquare;
-            left++;
-        } else {
-            result[index] = rightSquare;
-            right--;
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            nums[i] *= nums[i];
+            ans.push_back(nums[i]);
         }
-
-        index--;
-    }
-
-    return result;
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
