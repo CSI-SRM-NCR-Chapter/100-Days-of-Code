365c9eed1bd5f1d1a7c844235832f058d6d12895
Prashant Kumar Singh <81946035+Prashant160420@users.noreply.github.com>
2023-06-11 18:19:15 +0530

    Update 41a.cpp

--- Prashant Kumar Singh/day-41/41a.cpp
+++ Prashant Kumar Singh/day-41/41a.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        double product = 1;
-
-        for(int i=0; i<nums.size(); i++){
-            product *= nums[i]; 
-        }
-
-        if(product > 0) return 1;
-        else if (product < 0) return -1;
-        else return 0;
+        double prod = 1;
+        for(int i=0; i<nums.size(); i++)
+            prod *= nums[i];
+        if(prod > 0) return 1;
+        else if (prod < 0) return -1;
+        return 0;
     }
 };
