787ebf78c1d1359980afc903468390587d02b6fa
Prashant Kumar Singh <81946035+Prashant160420@users.noreply.github.com>
2023-06-11 21:40:17 +0530

    Update 46b.cpp

--- Prashant Kumar Singh/day-46/46b.cpp
+++ Prashant Kumar Singh/day-46/46b.cpp
@@ -2,29 +2,13 @@ class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
 
-        int n = strs.size();     // number of rows
-        int m = strs[0].size();  // number of columns
-
-        // count columns to be deleted
-        int columns = 0 ;
-
-        for(int i=0;i<m;i++){
-
-            for(int j=1;j<n;j++){
-
-                char currColChar = strs[j][i];        // character of current column
-                char previousColChar = strs[j-1][i];  // character of previous column
-
-                // check sorted or not
-                if(currColChar < previousColChar){
-                    columns++;
+        int ans=0;
+        for(int i=0; i<strs[0].size(); i++)
+            for(int j=1; j<strs.size(); j++)
+                if(strs[j][i] < strs[j-1][i]){
+                    ans++;
                     break;
                 }
-
-            }
-        }
-
-        // return number of columns to be deleted
-        return columns;
+                return ans;
     }
 };
