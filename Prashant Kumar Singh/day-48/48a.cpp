5a3b9ea1865b74521a7d70a7315d81bd71885772
Prashant Kumar Singh <81946035+Prashant160420@users.noreply.github.com>
2023-06-27 12:12:49 +0530

    Update 48a.cpp

--- Prashant Kumar Singh/day-48/48a.cpp
+++ Prashant Kumar Singh/day-48/48a.cpp
@@ -1,29 +1,14 @@
-/** 
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * int guess(int num);
- */
-
 class Solution {
 public:
     int guessNumber(int n) {
-        int s=1,e=n;
-        int mid = s+(e-s)/2;
+        int start = 1, end = n;
+        int mid = start + (end - start)/2;
 
-        while(s <= e ){
-            if(guess(mid)==0){
-                return mid;
-            }
-            else if(guess(mid)==-1){
-                e = mid - 1;
-            }
-            else{
-                s = mid + 1;
-            }
-            mid = s+(e-s)/2;
+        while(start <= end){
+            if(guess(mid)==0) return mid;
+            else if(guess(mid)==-1) end = mid - 1;
+            else start = mid + 1;
+            mid = start + (end - start)/2;
         }
         return 0;
     }
